26002e05d5a7d4ecc369f7ae9240be9d9bb5d0ae
Akim <akimkom@protonmail.com>
2025-06-17 19:39:47 +0200

    Make words fly into position

--- flying-letters/flying_letters.js
+++ flying-letters/flying_letters.js
@@ -1,74 +1,93 @@
-const leftNumberDiv = document.querySelector('#left-num');
-const rightNumberDiv = document.querySelector('#right-num');
 const inputBox = document.querySelector('#input-value');
-const checkButton = document.querySelector('#apply-button');
+const applyButton = document.querySelector('#apply-button');
 const gameArea = document.querySelector('.game-area');
-const scoreDiv = document.querySelector('#score');
-const fontSizeDiv = document.querySelector('#fontsize');
+const gameTextDiv = document.querySelector('#gametext-div');
 
-const maxInt = 99;
+const phrase = 'Летающие буквы, но на самом деле слова!';
+let offscreenWords = []
 
-function getRandomInt(max) {
-    return Math.ceil(Math.random() * max);
+gameTextDiv.textContent = '';
+gameTextDiv.style.opacity = 0;
+
+phrase.split(' ').forEach(word => {
+    const span = document.createElement('span');
+    span.textContent = word;
+    span.className = 'offscreen word';
+    span.style.position = 'absolute';
+    document.body.appendChild(span);
+    gameTextDiv.innerHTML += '<span>' + word + ' </span>';
+    offscreenWords.push(span);
+});    
+
+
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function spawnOffscreen(word) {
+    const rect = word.getBoundingClientRect();
+    const wordWidth = rect.width;
+    const wordHeight = rect.height;
+    const gameAreaWidth = gameArea.getBoundingClientRect().width;
+    const gameAreaHeight = gameArea.getBoundingClientRect().height;
+
+    const edge = randInt(0, 3);
+    const top = 0;
+    const right = 1;
+    const bottom = 2;
+    const left = 3;
+    const offset = 50;
+    let x,y;
+
+    switch (edge) {
+        case top:
+            x = randInt(-wordWidth, gameAreaWidth+offset);
+            y = -wordHeight;
+            break;
+        case bottom:
+            x = randInt(-wordWidth, gameAreaWidth+offset);
+            y = gameAreaHeight;
+            break;
+        case right:
+            x = gameAreaWidth;
+            y = randInt(-wordHeight, gameAreaHeight+offset);
+            break;
+        case left:
+            x = -wordWidth;
+            y = randInt(-wordHeight, gameAreaHeight+offset);
+            break;
+    }
+    
+    word.style.left = x + 'px';
+    word.style.top = y + 'px';
+
+    return;
 }
 
 
-function assignRandomNumbers(maxInt) {
-    leftNumberDiv.innerText = getRandomInt(maxInt);
-    rightNumberDiv.innerText = getRandomInt(maxInt);
+offscreenWords.forEach(word => {
+    spawnOffscreen(word);
+});
+
+const timeline = gsap.timeline({ paused: true });
+
+// These should always be equal, but just in case.
+const numOfWords = Math.min(offscreenWords.length, gameTextDiv.children.length)
+const transparentWords = gameTextDiv.children
+
+
+for (let i = 0; i < numOfWords; i++) {
+    const from = offscreenWords[i].getBoundingClientRect();
+    const to = transparentWords[i].getBoundingClientRect();
+
+    const dx = to.left - from.left;
+    const dy = to.top - from.top;
+
+    timeline.to(offscreenWords[i], {
+        x: `+=${dx}`,
+        y: `+=${dy}`,
+        duration: 1,
+    }, 0); // 0 means all animate simultaneously
 }
 
-
-function colourNumbers(colour) {
-    leftNumberDiv.style.color = colour;
-    rightNumberDiv.style.color = colour;
-}
-
-
-window.addEventListener('load', function () {
-    assignRandomNumbers(maxInt);
-})
-
-
-fontSizeDiv.addEventListener('input', function () {
-    leftNumberDiv.style.fontSize = parseInt(fontSizeDiv.value) + 'px';
-    rightNumberDiv.style.fontSize = parseInt(fontSizeDiv.value) + 'px';
-})
-
-checkButton.addEventListener('click', function () {
-    const input = inputBox.value.trimEnd().split(' ');
-    const leftNumber = parseInt(leftNumberDiv.innerText);
-    const rightNumber = parseInt(rightNumberDiv.innerText);
-
-    if (input.length !== 2) {
-        alert('Введите два числа, разделенные пробелом.');
-        return;
-    }
-
-    colourNumbers('black');
-
-    const leftInput = parseInt(input[0]);
-    const rightInput = parseInt(input[1]);
-
-    if (leftInput === leftNumber && rightInput === rightNumber) {
-        const currentGap = parseInt(getComputedStyle(gameArea).getPropertyValue('gap'));
-        const distanceModifier = parseFloat(document.querySelector('#distance').value);
-        const delayInMs = parseFloat(document.querySelector('#delay').value);
-
-        gameArea.style.gap = currentGap + 5*distanceModifier + 'px';
-        assignRandomNumbers(maxInt);
-        inputBox.value = '';
-        inputBox.style.backgroundColor = "rgba(0, 255, 0, 0.3)";
-        scoreDiv.innerText = currentGap + 5*distanceModifier;
-        setTimeout(() => {colourNumbers('white')}, delayInMs);
-    } else {
-        inputBox.style.backgroundColor = "rgba(255, 0, 0, 0.3)";
-    }
-})
-
-
-inputBox.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        checkButton.click();
-    }
-})
+timeline.play();
