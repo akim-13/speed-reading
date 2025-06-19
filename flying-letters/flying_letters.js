958f56da3fab9af558e1e0f61c7cb1b6454e0bf6
Akim <akimkom@protonmail.com>
2025-06-19 18:59:03 +0200

    Major refactoring

--- flying-letters/flying_letters.js
+++ flying-letters/flying_letters.js
@@ -1,25 +1,19 @@
+const gameArea = document.querySelector('.game-area');
 const inputBox = document.querySelector('#input-value');
 const startButton = document.querySelector('#start-button');
-const gameArea = document.querySelector('.game-area');
 const gameTextDiv = document.querySelector('#gametext-div');
-
 const fontSizeDiv = document.querySelector('#fontsize');
 const speedDiv = document.querySelector('#speed');
 const splitIntoWordsCheckbox = document.querySelector('#splitIntoWords');
 
+
+// Set defatuls.
 const initialFontSize = parseInt(fontSizeDiv.value) + 'px';
 document.documentElement.style.setProperty('--word-font-size', initialFontSize);
-
-let duration = -speedDiv.value;
-
 // As oppposed to splitting into characters.
 let splitIntoWords = splitIntoWordsCheckbox.checked;
+let duration = -speedDiv.value;
 
-splitIntoWordsCheckbox.addEventListener('change', () => {
-    splitIntoWords = splitIntoWordsCheckbox.checked;
-});
-
-startButton.addEventListener('click', start);
 
 let isAnimationRunning = false;
 
@@ -28,21 +22,46 @@ async function start() {
 
     isAnimationRunning = true;
 
-    const text = inputBox.value;
-    const lines = text.split('\n');
-
+    const lines = inputBox.value.split('\n');
     for (const line of lines) {
         await animatePhrase(line);
     }
     
     isAnimationRunning = false;
+    
+    gameTextDiv.textContent = 'Введите текст и нажмите "Готово"';
+    gameTextDiv.style.opacity = 1;
 }
 
-function animatePhrase(phrase) {
-    let offscreenWords = []
 
+function animatePhrase(phrase) {
     gameTextDiv.textContent = '';
     gameTextDiv.style.opacity = 0;
+
+    let offscreenWords = splitPhraseIntoSpans(phrase);
+
+    offscreenWords.forEach(word => {
+        spawnSpansOffscreen(word);
+    });
+
+    const transparentWords = gameTextDiv.children
+    
+    timeline = defineAnimationTimeline(offscreenWords, transparentWords);
+    timeline.play();
+    
+    return new Promise(resolve => {
+        timeline.eventCallback('onComplete', () => {
+            // Clean-up.
+            offscreenWords.forEach(word => document.body.removeChild(word));
+            // Resolve the promise to start the next animation.
+            resolve();
+        });
+    });
+}
+
+
+function splitPhraseIntoSpans(phrase) {
+    let offscreenWords = [];
 
     const splitPhrase = splitIntoWords ? phrase.split(' ') : phrase.split('');
 
@@ -55,14 +74,60 @@ function animatePhrase(phrase) {
         offscreenWords.push(span);
     });
 
-    offscreenWords.forEach(word => {
-        spawnOffscreen(word);
-    });
+    return offscreenWords;
+}
 
-    const transparentWords = gameTextDiv.children
+
+function spawnSpansOffscreen(word) {
+    const rect = word.getBoundingClientRect();
+    const wordWidth = rect.width;
+    const wordHeight = rect.height;
+    const gameAreaWidth = gameArea.getBoundingClientRect().width;
+    const gameAreaHeight = gameArea.getBoundingClientRect().height;
+
+    const edge = generateRandomInt(0, 3);
+    const top = 0;
+    const right = 1;
+    const bottom = 2;
+    const left = 3;
+
+    const offset = 50;
+    let x,y;
+
+    switch (edge) {
+        case top:
+            x = generateRandomInt(-wordWidth, gameAreaWidth+offset);
+            y = -wordHeight;
+            break;
+
+        case bottom:
+            x = generateRandomInt(-wordWidth, gameAreaWidth+offset);
+            y = gameAreaHeight;
+            break;
+
+        case right:
+            x = gameAreaWidth;
+            y = generateRandomInt(-wordHeight, gameAreaHeight+offset);
+            break;
+
+        case left:
+            x = -wordWidth;
+            y = generateRandomInt(-wordHeight, gameAreaHeight+offset);
+            break;
+    }
+    
+    word.style.left = x + 'px';
+    word.style.top = y + 'px';
+
+    return;
+}
+
+
+function defineAnimationTimeline(offscreenWords, transparentWords) {
+    const timeline = gsap.timeline({ paused: true });
+    
     // These should always be equal, but just in case.
     const numOfWords = Math.min(offscreenWords.length, transparentWords.length)
-    const timeline = gsap.timeline({ paused: true });
 
     for (let i = 0; i < numOfWords; i++) {
         const from = offscreenWords[i].getBoundingClientRect();
@@ -85,70 +150,31 @@ function animatePhrase(phrase) {
             duration: duration/3,
             ease: "power1.out",
         }, duration/2); // Start `duration/2` seconds after motion begins.
-
     }
-
-    timeline.play();
     
-    return new Promise(resolve => {
-        timeline.eventCallback('onComplete', () => {
-            // Clean-up.
-            offscreenWords.forEach(word => document.body.removeChild(word));
-            // Resolve the promise to start the next animation.
-            resolve();
-        });
-    });
+    return timeline;
 }
 
-function randInt(min, max) {
+
+function generateRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function spawnOffscreen(word) {
-    const rect = word.getBoundingClientRect();
-    const wordWidth = rect.width;
-    const wordHeight = rect.height;
-    const gameAreaWidth = gameArea.getBoundingClientRect().width;
-    const gameAreaHeight = gameArea.getBoundingClientRect().height;
-
-    const edge = randInt(0, 3);
-    const top = 0;
-    const right = 1;
-    const bottom = 2;
-    const left = 3;
-    const offset = 50;
-    let x,y;
-
-    switch (edge) {
-        case top:
-            x = randInt(-wordWidth, gameAreaWidth+offset);
-            y = -wordHeight;
-            break;
-        case bottom:
-            x = randInt(-wordWidth, gameAreaWidth+offset);
-            y = gameAreaHeight;
-            break;
-        case right:
-            x = gameAreaWidth;
-            y = randInt(-wordHeight, gameAreaHeight+offset);
-            break;
-        case left:
-            x = -wordWidth;
-            y = randInt(-wordHeight, gameAreaHeight+offset);
-            break;
-    }
-    
-    word.style.left = x + 'px';
-    word.style.top = y + 'px';
-
-    return;
-}
 
 fontSizeDiv.addEventListener('input', () => {
   const newSize = parseInt(fontSizeDiv.value) + 'px';
   document.documentElement.style.setProperty('--word-font-size', newSize);
 });
 
+
 speedDiv.addEventListener('input', () => {
     duration = parseInt(-speedDiv.value);
 });
+
+
+splitIntoWordsCheckbox.addEventListener('change', () => {
+    splitIntoWords = splitIntoWordsCheckbox.checked;
+});
+
+
+startButton.addEventListener('click', start);
